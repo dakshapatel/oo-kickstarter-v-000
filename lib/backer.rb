57162ef87ea2f9100c715d94ee6eb73bf4c93815
Daksha Patel <dakshapatel100@gmail.com>
2018-05-15 04:07:18 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,12 +2,12 @@ class Backer
 
 attr_accessor :name, :backed_project
 
-
+@backed_projects = []
 def initialize(name)
   @name = name
 end
 
-def backed_project(project)
+def back_project(project)
   ProjectBacker.new(project, self)
 end
 
